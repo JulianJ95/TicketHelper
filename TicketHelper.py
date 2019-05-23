c62cd007db94b45b15209a6e23b6d79ad097cbb7
JulianJ95 <julian.samirx.jubran@intel.com>
2019-05-23 11:01:53 +0300

    Add radio button

--- TicketHelper.py
+++ TicketHelper.py
@@ -5,12 +5,12 @@ from tkinter import *
 
 ChromeDriverPath = os.path.join(os.getcwd(), "bin", "chromedriver.exe")
 
-
 def function():
-    Browser = webdriver.Chrome(ChromeDriverPath)
-    Browser.get('http://www.google.com/xhtml')
-window = Tk()
+    browser = webdriver.Chrome(ChromeDriverPath)
+    browser.get('http://www.google.com/xhtml')
 
+
+window = Tk()
 window.title("Welcome to LikeGeeks app")
 
 window.geometry('350x200')
@@ -20,8 +20,10 @@ lbl = Label(window, text="Hello")
 lbl.grid(column=0, row=0)
 
 btn = Button(window, text="Click Me", command=function)
-
 btn.grid(column=1, row=0)
+radiobtn = Radiobutton(window, text="Python1", value=0, command=function)
+
+radiobtn.grid(column=0, row=1)
+
 
 window.mainloop()
-
